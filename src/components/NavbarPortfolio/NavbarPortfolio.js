38ed6369a75b566572eab19fd79f211e52841cb9
Massimilianok <massimiliano.rizzuto87@gmail.com>
2021-02-16 14:34:36 +0100

    refactor: Update style of mobile nav

--- src/components/NavbarPortfolio/NavbarPortfolio.js
+++ src/components/NavbarPortfolio/NavbarPortfolio.js
@@ -1,22 +1,25 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import './NavbarPortfolio.css';
 
 const NavbarPortfolio = ({ children }) => {
-  window.onscroll = () => {
-    const navMobile = document.getElementById('navbarMobile');
-    window.scrollY !== 0
-      ? navMobile.classList.add('navbar-portfolio-scroll')
-      : navMobile.classList.remove('navbar-portfolio-scroll');
-  };
+  const navBarMobile = useRef();
+
+  useEffect(() => {
+    window.onscroll = () => {
+      window.scrollY !== 0
+        ? navBarMobile.current.classList.add('navbar-portfolio-scroll')
+        : navBarMobile.current.classList.remove('navbar-portfolio-scroll');
+    };
+  }, []);
 
   return (
     <div
       className="navbar-portfolio d-flex justify-content-center align-items-center d-lg-none d-block"
-      id="navbarMobile"
+      ref={navBarMobile}
     >
       {children}
     </div>
   );
-}
+};
 
 export default NavbarPortfolio;
